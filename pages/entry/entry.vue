c4595fa13292ac87b90dd8e3962fb624f11593a6
ZhengYanHu <yanhu.zheng@outlook.com>
2022-12-14 19:32:32 +0800

    update

---
+++ pages/entry/entry.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+		<view class="bg">
+			<image src='https://gitee.com/YanHuZheng/asset/raw/master/gif/guangsu.gif'></image>
+		</view>
+
+	</view>
+</template>
+
+<script>
+
+
+export default {
+	data() {
+		return {
+
+		};
+	},
+	onLoad(query) {
+		console.log(query);
+		
+	},
+	methods: {
+		
+	}
+};
+</script>
+
+<style lang="less">
+.content {
+	width: 100vw;
+	height: 100vh;
+}
+
+
+</style>
